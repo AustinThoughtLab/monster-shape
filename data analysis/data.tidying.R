887ad91f73f4b19eb4eadb6a025dd86dab36958f
belizjordan <brookejordan@utexas.edu>
2023-07-07 11:59:42 -0500

    Update data.tidying.R

--- data analysis/data.tidying.R
+++ data analysis/data.tidying.R
@@ -28,17 +28,80 @@ for(i in 1:length(files)){
 
 ## get rid of unnecessary columns and reorganize
 
+# misc rows added in by script
 df2 <- subset(df, trial_type!="video-button-response" & trial_type!= "fullscreen" & trial_type!= "preload"
-              & task_part != "demo")
+              & task_part != "demo" & task_part != "id-question" & task_part != "version" 
+              & stimulus != "<p>Now I have some questions for you.</p>"
+              & stimulus != "<p>Now, we’re going to choose a new game to play.</p><p>We’re going to look at different games, and for each set of games</p><p>you’ll choose the one you want to play the most.</p><p>Here are the options:</p>"
+              & stimulus != "<p>Alright, here are the next three options:</p>"
+              & stimulus != "<p>Alright, here are the last three options:</p>"
+              & stimulus != "<p>Let's play the next game!</p>"
+              & stimulus != "<p> Do you want to hear the instructions again or are you ready to play the game?</p>",
+)
 View(df2)
 
-# add pID col
+# fix pid
+df2$version <- df2$id
+
+colnames(df2)[colnames(df2) == "version"] ="participant.id"
+df2 <- df2 %>%
+  select(-id, -followup, -stimulus)
+
+# reorder columns
+col_order <- c("participant.id", "condition", "counterbalancing",
+               "order", "task_part", "response", "trial_type", "trial_index", "choices", "correct")
+lt <- df2[, col_order]
+
+## recode thingsss
+
+# rename counterbalancing and order 
+lt$counterbalancing <- recode(lt$counterbalancing, "C1" = "monster","C2" = "robot")
+
+lt$order <- lt$order %>%
+  str_remove("o")
+
+# shape game survey responses
+
+lt$response <- lt$response %>%
+  str_replace_all("A lot","3") %>%
+  str_replace_all("A little","2") %>%
+  str_replace_all("Not at all","1") %>%
+  str_replace_all("More each day","3") %>%
+  str_replace_all("Same each day","2") %>%
+  str_replace_all("Fewer each day","1") %>%
+  str_replace_all("More", "3") %>% 
+  str_replace_all("Same", "2") %>% 
+  str_replace_all("Less", "1") %>% 
+  str_remove("\\{\"liked\":\"") %>%
+  str_remove("\"\\}") %>%
+  str_remove("\\{\"learned\":\"") %>%
+  str_remove("\"\\}") %>%
+  str_remove("\\{\"stars\":\"") %>%
+  str_remove("\"\\}") %>%
+  str_remove("\\{\"learned-q\":\"") %>%
+  str_remove("\"\\}") %>%
+  str_remove(""{"guesses":"") %>% 
+  as.numeric()
+
+
+options <- df %>%
+  filter(task_part=="options") %>%
+  select(response,choices) %>%
+  filter(choices!='["Continue"]') %>%
+  filter(choices!='[]') %>%
+  mutate(choices2 = choices %>%
+           str_remove('\\[') %>%
+           str_remove('\"]') %>%
+           str_remove_all('img/opt/opt') %>%
+           str_remove_all('\"') %>%
+           str_remove_all('.png'))
 
 
 
 
-
-
+## if o1 and 2, then "option"
+## correct T/F standardized
+## task_part == day-1, task_part ==day-2, task_part == day=3 --> choices...
 
 
 
@@ -65,17 +128,3 @@ subdf <- df2 %>%
            task_part!= "followup-day-1" & task_part!="followup-day-2" & task_part!= "followup-day-3")
 
 View(subdf)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
