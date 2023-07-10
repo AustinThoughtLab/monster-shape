4b2a43ea606a5d824e6294818ebbb48139fca9c7
belizjordan <brookejordan@utexas.edu>
2023-07-10 11:46:16 -0500

    Update data.tidying.R

--- data analysis/data.tidying.R
+++ data analysis/data.tidying.R
@@ -61,7 +61,7 @@ lt$counterbalancing <- recode(lt$counterbalancing, "C1" = "monster","C2" = "robo
 lt$order <- lt$order %>%
   str_remove("o")
 
-# shape game survey responses
+# recoding responses
 
 lt$response <- lt$response %>%
   str_replace_all("A lot","3") %>%
@@ -81,9 +81,13 @@ lt$response <- lt$response %>%
   str_remove("\"\\}") %>%
   str_remove("\\{\"learned-q\":\"") %>%
   str_remove("\"\\}") %>%
-  str_remove("\\{\"guesses\":\"")
+  str_remove("\\{\"guesses\":\"") %>% 
+  str_remove("\\{\"choice-1\":\"") %>% 
+  str_remove("\\{\"choice-2\":\"") %>% 
+  str_remove("\\{\"choice-3\":\"")
+  
  
-# recode options to be their choices
+# just removing the misc characters from jspsych
 
 lt2 <- lt
 
@@ -99,42 +103,40 @@ lt2$choices <- lt2$choices %>%
 # after strsplit we should use something like nth(as.numeric(response)+1) to get the right item from the vector
 # need to fix vvv I did it the lazy way but Jimmy knows a better way to do it
 
-lt2.5 <- lt2 %>% 
-  filter(task_part== "options" & trial_type == "html-button-response2") %>% 
-  mutate(response = recode(response, "0" = substr(choices, 1, 2), 
-                           "1" = substr(choices, 4, 5), 
-                           "2" = substr(choices, 7, 8),
-                           .default = NA_character_))
+# recoding choices to be more intuitive 
 
-lt2.5$response <- lt2.5$response %>% 
-  str_replace_all("A1", "monkey-shape") %>% 
-  str_replace_all("A2", "monster-path") %>% 
-  str_replace_all("A3", "robot-path") %>% 
-  str_replace_all("A4", "monkey-path") %>% 
-  str_replace_all("B1", "alien-shape") %>% 
-  str_replace_all("B2", "monster-ball") %>% 
-  str_replace_all("B3", "robot-ball") %>% 
-  str_replace_all("B4", "alien-ball") %>% 
-  str_replace_all("C1", "monster-shape") %>% 
-  str_replace_all("C2", "robot-shape") %>% 
-  str_replace_all("C3", "monster-hiding") %>% 
-  str_replace_all("C4", "robot-hiding")
+lt2 <- lt2 %>%
+  mutate(response2 = ifelse(task_part=="options" &
+                              trial_type=="html-button-response2",
+                            recode(response, "0" = substr(choices, 1, 2), 
+                                   "1" = substr(choices, 4, 5), 
+                                   "2" = substr(choices, 7, 8),
+                                   .default = NA_character_),
+                            response
+                            ),
+         response3 = recode(response2,
+                            "A1" = "monkey-shape",
+                            "A2" = "monster-path",
+                            "A3" = "robot-path",
+                            "A4" = "monkey-path",
+                            "B1" = "alien-shape",
+                            "B2" = "monster-ball",
+                            "B3" = "robot-ball",
+                            "B4" = "alien-ball",
+                            "C1" = "monster-shape",
+                            "C2" = "robot-shape",
+                            "C3" = "monster-hiding",
+                            "C4" = "robot-hiding"
+                            )
+         )
 
-# need to merge lt2.5 -> lt2 regardless of differing row #s... ugggghhghghghgh
+# updating response col
 
-# not working -> f <- merge(x = lt2.5, y = lt2, by = "participant.id", all.x=TRUE)
+lt2 <- lt2 %>% 
+  select(-response2) %>% 
+  mutate(response = response3) %>%
+  select(-response3)
 
-lt3 <- lt2
-lt3.5 <- lt2.5
-
-lt3[match(lt3.5$participant.id, lt3$participant.id), ] <- lt3.5
-
-lt3[match(lt3.5$task_part, lt3$task_part, incomparables = NULL), ] <- lt3.5
-
-# join if participant ID matches and task_part = options AND trial_type = html-button-response-2 
-
-
-View(lt3)
 
 
 
@@ -214,3 +216,58 @@ subdf <- df2 %>%
            task_part!= "followup-day-1" & task_part!="followup-day-2" & task_part!= "followup-day-3")
 
 View(subdf)
+
+
+
+
+
+
+
+
+
+
+
+
+############################## GRAVEYARD #######################################
+
+lt2.5 <- lt2 %>% 
+  filter(task_part== "options" & trial_type == "html-button-response2") %>% 
+  mutate(response = recode(response, "0" = substr(choices, 1, 2), 
+                           "1" = substr(choices, 4, 5), 
+                           "2" = substr(choices, 7, 8),
+                           .default = NA_character_))
+
+lt2.5$response <- lt2.5$response %>% 
+  str_replace_all("A1", "monkey-shape") %>% 
+  str_replace_all("A2", "monster-path") %>% 
+  str_replace_all("A3", "robot-path") %>% 
+  str_replace_all("A4", "monkey-path") %>% 
+  str_replace_all("B1", "alien-shape") %>% 
+  str_replace_all("B2", "monster-ball") %>% 
+  str_replace_all("B3", "robot-ball") %>% 
+  str_replace_all("B4", "alien-ball") %>% 
+  str_replace_all("C1", "monster-shape") %>% 
+  str_replace_all("C2", "robot-shape") %>% 
+  str_replace_all("C3", "monster-hiding") %>% 
+  str_replace_all("C4", "robot-hiding")
+
+# need to merge lt2.5 -> lt2 regardless of differing row #s... ugggghhghghghgh
+
+# not working -> 
+
+f <- merge(x = lt3.5, y = lt3, by = "participant.id", all.x=TRUE)
+
+
+
+lt3 <- lt2
+lt3.5 <- lt2.5
+
+lt3[match(lt3.5$participant.id, lt3$participant.id), ] <- lt3.5
+
+lt3[match(lt3.5$task_part, lt3$task_part, incomparables = NULL), ] <- lt3.5
+
+# join if participant ID matches and task_part = options AND trial_type = html-button-response-2 
+# "participant.id", "condition", "counterbalancing", "order", "task_part", "response", "trial_type", "trial_index", "choices", "correct"
+
+
+
